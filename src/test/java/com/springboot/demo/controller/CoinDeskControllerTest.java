dfa4e8b86bfc7c50f42f79f097a9ffe44c5ec37e
jin809266 <30747512+jin809266@users.noreply.github.com>
2021-07-23 23:27:59 +0800

    format

--- src/test/java/com/springboot/demo/controller/CoinDeskControllerTest.java
+++ src/test/java/com/springboot/demo/controller/CoinDeskControllerTest.java
@@ -28,46 +28,37 @@ public class CoinDeskControllerTest{
 
 	// 1. test get coinDesk data by coinCode
 	@Test
-	public void getCoinDeskAPI() throws Exception 
-	{
-	      mockMvc.perform( MockMvcRequestBuilders
-	      .get("/coinDesks/{coinCode}", "USD")
-	      .accept(MediaType.APPLICATION_JSON))
-	      .andDo(print())
-	      .andExpect(status().isOk())
-	      .andExpect(MockMvcResultMatchers.jsonPath("$.coinCode").value("USD"))
-	      .andExpect(MockMvcResultMatchers.jsonPath("$.coinRate").exists());
+	public void getCoinDeskAPI() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/coinDesks/{coinCode}", "USD").accept(MediaType.APPLICATION_JSON))
+			   .andDo(print()).andExpect(status().isOk())
+			   .andExpect(MockMvcResultMatchers.jsonPath("$.coinCode").value("USD"))
+			   .andExpect(MockMvcResultMatchers.jsonPath("$.coinRate").exists());
 	}
 	
 	
 	// 2. test add coinDesk data
 	@Test
 	public void testAddCoinDesk() throws Exception {
-
 		JSONObject request = new JSONObject().put("coinCode", "JPY").put("coinRate", 0.25);
 		mockMvc.perform(MockMvcRequestBuilders
-		.post("/coinDesk")
-		.content(request.toString())
-		.contentType(MediaType.APPLICATION_JSON)
-		.accept(MediaType.APPLICATION_JSON))
-		.andDo(print())
-        .andExpect(status().isCreated());
+			   .post("/coinDesk")
+			   .content(request.toString())
+			   .contentType(MediaType.APPLICATION_JSON)
+			   .accept(MediaType.APPLICATION_JSON))
+			   .andDo(print())
+			   .andExpect(status().isCreated());
 	}
 	
 	// 3. test update coinDesk data 
 	@Test
-	public void updateCoinDeskAPI() throws Exception 
-	{
-		   JSONObject request = new JSONObject().put("coinCode", "USD").put("coinRate", 0.26);
-		   mockMvc.perform( MockMvcRequestBuilders
-	      .put("/coinDesk/USD", 0.26)
-	      .content(request.toString())
-	      .contentType(MediaType.APPLICATION_JSON)
-	      .accept(MediaType.APPLICATION_JSON))
-	      .andExpect(status().isOk())
-	      .andExpect(MockMvcResultMatchers.jsonPath("$.coinCode").value(request.getString("coinCode")))
-	      .andExpect(MockMvcResultMatchers.jsonPath("$.coinRate").value(request.getInt("coinRate")))
-		  .andDo(print());
+	public void updateCoinDeskAPI() throws Exception {
+		JSONObject request = new JSONObject().put("coinCode", "USD").put("coinRate", 0.26);
+		mockMvc.perform(MockMvcRequestBuilders
+			   .put("/coinDesk/USD", 0.26).content(request.toString())
+			   .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+			   .andExpect(MockMvcResultMatchers.jsonPath("$.coinCode").value(request.getString("coinCode")))
+			   .andExpect(MockMvcResultMatchers.jsonPath("$.coinRate").value(request.getInt("coinRate")))
+			   .andDo(print());
 	}
 	
 	// 4. test delete coinDesk data by coinCode
